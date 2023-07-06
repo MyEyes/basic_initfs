502eef76a83baa8f4dff2d4b0eeaef47fff71241
Nils Ole Timm <not@firzen.de>
2023-07-06 18:01:27 +0200

    Added new symlinks, cleanly shutting down system after init exits

--- setup.sh
+++ setup.sh
@@ -2,5 +2,13 @@
 echo "Download busybox"
 curl -L 'https://www.busybox.net/downloads/binaries/1.26.2-defconfig-multiarch/busybox-x86_64' > ./root/bin/busybox
 chmod +x ./root/bin/busybox
-ln -s ./root/bin/busybox ./root/bin/sh
-ln -s ./root/bin/busybox ./root/bin/mount
+echo "Creating symlinks"
+cd ./root/bin/
+ln -s ./busybox ./sh
+ln -s ./busybox ./mount
+ln -s ./busybox ./ls
+ln -s ./busybox ./id
+ln -s ./busybox ./shutdown
+ln -s ./busybox ./ps
+ln -s ./busybox ./poweroff
+cd ../../
